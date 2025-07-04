44abe784bfc73d1358718e68cf4257c47b8088aa
frumbert <592257+frumbert@users.noreply.github.com>
2025-07-04 16:00:45 +1000

    Update filter.php for php 8.2+

--- filter.php
+++ filter.php
@@ -16,30 +16,35 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * This filter finds hyperlinks that wrap an image whose url is one of the activites in the course
- * when it matches, it adds a span/icon inside the hyperlink with the completion state of the activity
- * the span/icon can be styled via the theme's custom css
+ * Adds a completion state icon to activity image links in course content.
  *
- * @package    filter
- * @subpackage activitycompletionimage
- * @copyright  2018 tim.stclair@gmail.com
+ * @package    filter_activitycompletionimage
+ * @copyright  2018-2025 Tim St Clair <tim.stclair@gmail.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 require_once("{$CFG->libdir}/completionlib.php");
-/**
- * Activity name filtering
- */
-class filter_activitycompletionimage extends moodle_text_filter {
-    // Trivial-cache - keyed on $cachedcourseid and $cacheduserid.
-    static $activitylist = null;
-    static $cachedcourseid;
-    static $cacheduserid;
 
-    function filter($text, array $options = array()) {
-        global $USER, $COURSE; // Since 2.7 we can finally start using globals in filters.
+class filter_activitycompletionimage extends moodle_text_filter {
+    // Simple cache keyed on course and user.
+    private static $activitylist = null;
+    private static $cachedcourseid;
+    private static $cacheduserid;
+
+    private static function get_relative_url($url) {
+      if (preg_match('#https?://[^/]+(/.*)$#i', $url, $matches)) {
+          return $matches[1];
+      }
+      if (strpos($url, '/') === 0) {
+          return $url;
+      }
+      return '';
+    }
+
+    public function filter($text, array $options = []): string {
+        global $USER, $COURSE;
 
         $coursectx = $this->context->get_course_context(false);
         if (!$coursectx) {
@@ -47,79 +52,85 @@ class filter_activitycompletionimage extends moodle_text_filter {
         }
         $courseid = $coursectx->instanceid;
 
-        // Initialise/invalidate our trivial cache if dealing with a different course.
+        // Invalidate cache if course or user changes.
         if (!isset(self::$cachedcourseid) || self::$cachedcourseid !== (int)$courseid) {
             self::$activitylist = null;
         }
         self::$cachedcourseid = (int)$courseid;
-        // And the same for user id.
+
         if (!isset(self::$cacheduserid) || self::$cacheduserid !== (int)$USER->id) {
             self::$activitylist = null;
         }
         self::$cacheduserid = (int)$USER->id;
 
-        /// It may be cached
-
+        // Build activity list if not cached.
         if (is_null(self::$activitylist)) {
-            self::$activitylist = array();
+            self::$activitylist = [];
             $modinfo = get_fast_modinfo($courseid);
             $completioninfo = new completion_info($COURSE);
             if (!empty($modinfo->cms)) {
                 foreach ($modinfo->cms as $cm) {
-                    if ($cm->visible and $cm->has_view()) { //  and $cm->uservisible) {
+                    if ($cm->visible && $cm->has_view()) {
                         $actinfo = $completioninfo->get_data($cm, false, self::$cacheduserid);
-                        self::$activitylist[$cm->url->out()] = (object)array(
+                        $urlpath = self::get_relative_url($cm->url->out());
+                        self::$activitylist[$urlpath] = (object)[
                             'name' => $cm->name,
                             'id' => $cm->id,
                             'uservisible' => $cm->uservisible,
                             'completed' => $actinfo->completionstate,
-                        );
+                        ];
                     }
                 }
             }
-            $foundLinks = array_keys(self::$activitylist);
-            $dom = new DOMDocument();
-            if (extension_loaded('mbstring')) {
-                $encoding = '';
-                $text  = mb_convert_encoding($text , 'HTML-ENTITIES', 'UTF-8'); // require mb_string
-            } else {
-                $encoding = '<?xml encoding="UTF-8">';
-            }
-
-            $dom->loadHTML($encoding . $text, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-            $hrefs = $dom->getElementsByTagName("a");
-            foreach ($hrefs as $href) {
-                if (!array_key_exists($href->getAttribute("href"), self::$activitylist)) continue;
-                $info = self::$activitylist[$href->getAttribute("href")]; // find this <a href> in the activity list
-                if (isset($info) && $href->hasChildNodes()) { // if this hyperlink is found in the activity list and its got child nodes
-                    $img = $href->getElementsByTagName("img");
-                    if ($img->length === 1) { // and those child nodes are an image
-                        $img[0]->removeAttribute("style"); //
-                        $i = $dom->createElement("i");
-                        $span = $dom->createElement("span");
-                        $href->setAttribute("data-cmid", $info->id);
-                        $href->setAttribute("class","completion-info");
-                        $span->setAttribute("title",get_string("incomplete", "filter_activitycompletionimage"));
-                        $span->setAttribute("class","state-incomplete");
-                        $icon = "fa-circle-o";
-                        if (!$info->uservisible) {
-                            $icon = "fa-ban";
-                            $span->setAttribute("class","state-unavailable");
-                            $span->setAttribute("title",get_string("unavailable", "filter_activitycompletionimage"));
-                        }
-                        if ($info->completed) {
-                            $icon = "fa-check-circle-o";
-                            $span->setAttribute("class","state-completed");
-                            $span->setAttribute("title",get_string("completed", "filter_activitycompletionimage"));
-                        }
-                        $i->setAttribute("class","fa {$icon}");
-                        $span->appendChild($i);
-                        $href->insertBefore($span, $img[0]);
-                    }
-                }
-            }
-            $text = $dom->saveHTML();
         }
-        return $text;
+
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument();
+
+        // Convert to HTML entities for DOM parsing (PHP 8.4 compatible).
+        $text = mb_convert_encoding($text, 'UTF-8', 'auto');
+        // $text = htmlspecialchars($text, ENT_QUOTES | ENT_HTML5, 'UTF-8', false);
+
+        $dom->loadHTML($text, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        libxml_clear_errors();
+
+        $hrefs = $dom->getElementsByTagName("a");
+        foreach ($hrefs as $href) {
+            $hrefkey = trim($href->getAttribute("href"));
+            $hrefpath = self::get_relative_url($hrefkey);
+            if (!array_key_exists($hrefpath, self::$activitylist)) {
+                continue;
+            }
+            $info = self::$activitylist[$hrefpath];
+            if ($info && $href->hasChildNodes()) {
+                $img = $href->getElementsByTagName("img");
+                if ($img->length === 1) {
+                    $img[0]->removeAttribute("style");
+                    $href->setAttribute("data-cmid", $info->id);
+                    $href->setAttribute("class", "completion-info");
+
+                    $span = $dom->createElement("span");
+                    $span->setAttribute("title", get_string("incomplete", "filter_activitycompletionimage"));
+                    $span->setAttribute("class", "state-incomplete");
+
+                    $icon = "fa-circle-o";
+                    if (!$info->uservisible) {
+                        $icon = "fa-ban";
+                        $span->setAttribute("class", "state-unavailable");
+                        $span->setAttribute("title", get_string("unavailable", "filter_activitycompletionimage"));
+                    } else if ($info->completed) {
+                        $icon = "fa-check-circle-o";
+                        $span->setAttribute("class", "state-completed");
+                        $span->setAttribute("title", get_string("completed", "filter_activitycompletionimage"));
+                    }
+
+                    $i = $dom->createElement("i");
+                    $i->setAttribute("class", "fa {$icon}");
+                    $span->appendChild($i);
+                    $href->insertBefore($span, $img[0]);
+                }
+            }
+        }
+        return $dom->saveHTML();
     }
 }
