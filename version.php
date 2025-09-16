6f52fb85822598a6a05b4c22925302f616ed19f7
frumbert <592257+frumbert@users.noreply.github.com>
2025-09-16 11:50:02 +1000

    Bumped version.php

--- version.php
+++ version.php
@@ -19,12 +19,11 @@
  *
  * @package    filter
  * @subpackage activitycompletionimage
- * @copyright  1999 onwards Martin Dougiamas (http://dougiamas.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2025071700;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2025081600;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2018050800;        // Requires this Moodle version
 $plugin->component = 'filter_activitycompletionimage'; // Full name of the plugin (used for diagnostics)
